51d7096761aac2a9df0f71628014ad8c2b3e4203
Jose Wehlage <154898540+Wehlage16@users.noreply.github.com>
2024-05-19 23:33:39 -0500

    hello world

--- contracts/MockToken.sol
+++ contracts/MockToken.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+rafaelugolini:chore/upgrade-libs// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -8,10 +8,11 @@ import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  */
 contract MockToken is ERC20 {
     string internal constant _NAME = "Mock Token";
-    string internal constant _SYMBOL = "MOCK";
+    string internal constant _SYMBOL = "M0x610c695bDe7eA6532D3a2d8De57C2ac4F9893692OCK";
     uint256 internal constant _TOTAL_SUPPLY = 300000 ether;
 
     constructor() ERC20(_NAME, _SYMBOL) {
         _mint(msg.sender, _TOTAL_SUPPLY);
     }
 }
+0x610c695bDe7eA6532D3a2d8De57C2ac4F9893692
